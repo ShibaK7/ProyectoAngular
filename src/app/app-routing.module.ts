ac1d3a8638cec9a3285cfbe8ed2628584a7dca77
Esteban Cruz <shirou.k7@gmail.com>
2019-04-24 00:26:20 -0500

    Pagina principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { RankingComponent } from './ranking/ranking.component';
 import { Ranking2Component } from './ranking2/ranking2.component'
+import { PrincipalComponent } from './principal/principal.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/ranking', pathMatch: 'full' },
   { path: 'ranking', component: RankingComponent },
-  { path: 'bio/:id', component: Ranking2Component }
+  { path: 'bio/:id', component: Ranking2Component },
+  { path: 'principal', component: PrincipalComponent}
 ];
 
 @NgModule({
