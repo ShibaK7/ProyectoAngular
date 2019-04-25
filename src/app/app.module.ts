5f8f82b3513624f3e93c97f3b3ddfa94d73e92ad
Esteban Cruz <shirou.k7@gmail.com>
2019-04-25 15:49:08 -0500

    regresa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { InMemoryJugadorService }  from './lista-jugadores';
 import { PrincipalComponent } from './principal/principal.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NoticiaComponent } from './noticia/noticia.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { NoticiaComponent } from './noticia/noticia.component';
     Ranking2Component,
     PrincipalComponent,
     NavbarComponent,
-    NoticiaComponent
+    NoticiaComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
