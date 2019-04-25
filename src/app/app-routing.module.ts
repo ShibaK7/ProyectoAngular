0841e4e1f02442c22f7d7930917eba3d947c0457
Esteban Cruz <shirou.k7@gmail.com>
2019-04-24 19:31:17 -0500

    router link actualizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { PrincipalComponent } from './principal/principal.component';
 import { RankingComponent } from './ranking/ranking.component';
 import { Ranking2Component } from './ranking2/ranking2.component'
-import { PrincipalComponent } from './principal/principal.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/ranking', pathMatch: 'full' },
+  { path: '', redirectTo: '/principal', pathMatch: 'full' },
   { path: 'ranking', component: RankingComponent },
   { path: 'bio/:id', component: Ranking2Component },
   { path: 'principal', component: PrincipalComponent}
