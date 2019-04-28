74cf2d97e356b31da783a0b6df0ed88a3b00c890
Rebeca <reberuqu98@gmail.com>
2019-04-27 19:58:10 -0500

    ResultadosIndividuales

--- src/app/resultados/completos/completos.component.ts
+++ src/app/resultados/completos/completos.component.ts
@@ -16,12 +16,16 @@ export class CompletosComponent implements OnInit {
 
   private tipo: string = "";
   private generoSeleccionado: string = null;
-  private registrosPorPagina: number = 7;
+  private registrosPorPagina: number = 5;
   private numeroPaginaI: number = 1;
   private numeroPaginaD: number = 1;
+  private numeroPaginasFiltradasI: number = 1;
+  private numeroPaginasFiltradasD: number = 1;
+  private totalPaginasFiltradasI: number;
+  private totalPaginasFiltradasD: number;
   private totalPaginasI: number;
   private totalPaginasD: number;
-  private filtradoGenerp: boolean = false;
+  private filtradoGenero: boolean = false;
 
   individuales: ResultadoI[];
   individuales$: Observable<ResultadoI[]>;
@@ -46,16 +50,35 @@ export class CompletosComponent implements OnInit {
 
   getResultadoIndividual(): void
   {
-    let indice = (this.numeroPaginaI -1)*this.registrosPorPagina;
-    this.resultadoIService.getJugadores().subscribe( individuales =>
+    if(!this.filtradoGenero)
+    {
+      let indice = (this.numeroPaginaI -1)*this.registrosPorPagina;
+      this.resultadoIService.getJugadores().subscribe( individuales =>
       this.individuales = individuales.filter(individual => this.generoSeleccionado == null || this.generoSeleccionado == individual.genero).slice(indice, indice+this.registrosPorPagina)) 
+    }
+    else
+    {
+      let indice = (this.numeroPaginasFiltradasI -1)*this.registrosPorPagina;
+      this.resultadoIService.getJugadores().subscribe( individuales =>
+      this.individuales = individuales.filter(individual => this.generoSeleccionado == null || this.generoSeleccionado == individual.genero).slice(indice, indice+this.registrosPorPagina)) 
+    }
   }
 
   getResultadoDobles(): void
   {
-    let indice = (this.numeroPaginaD -1)*this.registrosPorPagina;
-    this.resultadoDService.getJugadores().subscribe( dobles =>
+    if(!this.filtradoGenero)
+    {
+      let indice = (this.numeroPaginaD -1)*this.registrosPorPagina;
+      this.resultadoDService.getJugadores().subscribe( dobles =>
       this.dobles = dobles.filter(doble => this.generoSeleccionado == null || this.generoSeleccionado == doble.genero).slice(indice, indice+this.registrosPorPagina))
+    }
+    else
+    {
+      let indice = (this.numeroPaginasFiltradasD -1)*this.registrosPorPagina;
+      this.resultadoDService.getJugadores().subscribe( dobles =>
+      this.dobles = dobles.filter(doble => this.generoSeleccionado == null || this.generoSeleccionado == doble.genero).slice(indice, indice+this.registrosPorPagina))
+    }
+    
   }
 
   get generoIndividuales() : string[] {
@@ -64,12 +87,48 @@ export class CompletosComponent implements OnInit {
 
   getPaginasIndividual(): void {
     this.resultadoIService.getJugadores().subscribe( individuales =>
-      this.totalPaginasI = individuales.length / this.registrosPorPagina)
+      {
+        if(((individuales.length/this.registrosPorPagina)-(Math.trunc(individuales.length/this.registrosPorPagina)))==0)
+          this.totalPaginasI = Math.trunc(individuales.length / this.registrosPorPagina)
+        else
+        this.totalPaginasI = Math.trunc(individuales.length / this.registrosPorPagina)+1
+      }
+    )
+  }
+
+  getPaginasFiltradasIndividuales(): void {
+    this.resultadoIService.getJugadores().subscribe( individuales =>
+      {
+        if(((individuales.filter(individual => this.generoSeleccionado == null || this.generoSeleccionado == individual.genero).length/this.registrosPorPagina)-(Math.trunc(individuales.filter(individual => this.generoSeleccionado == null || this.generoSeleccionado == individual.genero).length/this.registrosPorPagina)))==0)
+          this.totalPaginasFiltradasI = Math.trunc(individuales.filter(individual => this.generoSeleccionado == null || this.generoSeleccionado == individual.genero).length / this.registrosPorPagina)
+        else
+          this.totalPaginasFiltradasI = Math.trunc(individuales.filter(individual => this.generoSeleccionado == null || this.generoSeleccionado == individual.genero).length / this.registrosPorPagina)+1
+
+          console.log(this.totalPaginasFiltradasI);
+      }
+    )
   }
 
   getPaginasDoble(): void {
     this.resultadoDService.getJugadores().subscribe( dobles =>
-      this.totalPaginasD = dobles.length / this.registrosPorPagina)
+      {
+        if(((dobles.length/this.registrosPorPagina)-(Math.trunc(dobles.length/this.registrosPorPagina)))==0)
+          this.totalPaginasD = Math.trunc(dobles.length / this.registrosPorPagina)
+        else
+          this.totalPaginasD = Math.trunc(dobles.length / this.registrosPorPagina)+1
+      }
+    )
+  }
+
+  getPaginasFiltradasDobles(): void {
+    this.resultadoDService.getJugadores().subscribe( dobles =>
+      {
+        if(((dobles.filter(doble => this.generoSeleccionado == null || this.generoSeleccionado == doble.genero).length/this.registrosPorPagina)-(Math.trunc(dobles.length/this.registrosPorPagina)))==0)
+          this.totalPaginasD = Math.trunc(dobles.filter(doble => this.generoSeleccionado == null || this.generoSeleccionado == doble.genero).length / this.registrosPorPagina)
+        else
+          this.totalPaginasD = Math.trunc(dobles.filter(doble => this.generoSeleccionado == null || this.generoSeleccionado == doble.genero).length / this.registrosPorPagina)+1
+      }
+    )
   }
 
   get generoDobles() : string[] {
@@ -84,48 +143,88 @@ export class CompletosComponent implements OnInit {
     if(newGeneroSeleccionado=="")
     {
       this.generoSeleccionado=null;
-      this.filtradoGenerp=false;
+      this.filtradoGenero=false;
+      this.numeroPaginasFiltradasD=1;
+      this.numeroPaginasFiltradasI=1;
       this.getResultadoIndividual();
       this.getResultadoDobles();
     }
     else
     {
       this.generoSeleccionado = newGeneroSeleccionado;
-      this.filtradoGenerp=true;
+      this.filtradoGenero=true;
       this.numeroPaginaI=1;
       this.numeroPaginaD=1;
+      this.getPaginasFiltradasIndividuales();
+      this.getPaginasFiltradasDobles();
       this.getResultadoIndividual();
       this.getResultadoDobles();
     }
   }
 
   avanzarPagina(tipo: number){
-    if(tipo===1)
+    if(tipo===1 )
     {
-      if(this.numeroPaginaI < this.totalPaginasI)
+      if(!this.filtradoGenero)
+      {
+        if(this.numeroPaginaI < this.totalPaginasI)
          this.numeroPaginaI++;
          this.getResultadoIndividual();
+      }
+      else
+      {
+        if(this.numeroPaginasFiltradasI < this.totalPaginasFiltradasI)
+         this.numeroPaginasFiltradasI++;
+         this.getResultadoIndividual();
+      }
     }
     else
     {
-      if(this.numeroPaginaD < this.totalPaginasD)
+      if(!this.filtradoGenero)
+      {
+        if(this.numeroPaginaD < this.totalPaginasD)
         this.numeroPaginaD++;
         this.getResultadoDobles();
+      }
+      else
+      {
+        if(this.numeroPaginasFiltradasD < this.totalPaginasFiltradasD)
+        this.numeroPaginasFiltradasD++;
+        this.getResultadoDobles();
+      }
     }
   }
 
   regresarPagina(tipo: number) {
     if(tipo===1)
     {
-      if(this.numeroPaginaI > 1)
-      this.numeroPaginaI--;
-      this.getResultadoIndividual();
+      if(!this.filtradoGenero)
+      {
+        if(this.numeroPaginaI > 1)
+        this.numeroPaginaI--;
+        this.getResultadoIndividual();
+      }
+      else
+      {
+        if(this.numeroPaginasFiltradasI > 1)
+        this.numeroPaginasFiltradasI--;
+        this.getResultadoIndividual();
+      }
     }
     else
     {
-      if(this.numeroPaginaD > 1)
-      this.numeroPaginaD--;
-      this.getResultadoDobles();
+      if(!this.filtradoGenero)
+      {
+        if(this.numeroPaginaD > 1)
+        this.numeroPaginaD--;
+        this.getResultadoDobles();
+      }
+      else
+      {
+        if(this.numeroPaginasFiltradasD > 1)
+        this.numeroPaginasFiltradasD--;
+        this.getResultadoDobles();
+      }
     }
   }
 
@@ -133,25 +232,76 @@ export class CompletosComponent implements OnInit {
   {
     if(tipo===1)
     {
-      this.numeroPaginaI = this.totalPaginasI;
-      this.getResultadoIndividual();
+      if(!this.filtradoGenero)
+      {
+        this.numeroPaginaI = this.totalPaginasI;
+        this.getResultadoIndividual();
+      }
+      else
+      {
+        this.numeroPaginasFiltradasI = this.totalPaginasFiltradasI;
+        this.getResultadoIndividual();
+      }
     }
     else
     {
-      this.numeroPaginaD = this.totalPaginasD;
-      this.getResultadoDobles();
+      if(!this.filtradoGenero)
+      {
+        this.numeroPaginaD = this.totalPaginasD;
+        this.getResultadoDobles();
+      }
+      else
+      {
+        this.numeroPaginasFiltradasD = this.totalPaginasFiltradasD;
+        this.getResultadoDobles();
+      }
     }
   }
 
   regresarPrincipio(tipo: number){
     if(tipo===1)
     {
-      this.numeroPaginaI = 1;
+      if(!this.filtradoGenero)
+      {
+        this.numeroPaginaI = 1;
+        this.getResultadoIndividual();
+      }
+      else
+      {
+        this.numeroPaginasFiltradasI = 1;
+        this.getResultadoIndividual();
+      }
+    }
+    else
+    {
+      if(!this.filtradoGenero)
+      {
+        this.numeroPaginaD = 1;
+        this.getResultadoDobles();
+      }
+      else
+      {
+        this.numeroPaginasFiltradasD = 1;
+        this.getResultadoDobles();
+      }
+    }
+  }
+
+  resetTipo(tipoSeleccionado: number) {
+    if(tipoSeleccionado===1)
+    {
+      this.tipo="individual";
+      this.generoSeleccionado=null;
+      this.numeroPaginaI=1;
+      this.filtradoGenero=false;
       this.getResultadoIndividual();
     }
     else
     {
-      this.numeroPaginaD = 1;
+      this.tipo="dobles";
+      this.generoSeleccionado=null;
+      this.numeroPaginaD=1;
+      this.filtradoGenero=false;
       this.getResultadoDobles();
     }
   }
--- src/app/resultadosI.ts
+++ src/app/resultadosI.ts
@@ -7,9 +7,14 @@ export class ResultadoI
     jugador1: string;
     bandera2: string;
     jugador2: string;
-    uno: string;
-    dos: string;
-    tres: string;
-    cuatro: string;
-    cinco: string;
+    j1s1: number;
+    j2s1: number;
+    j1s2: number;
+    j2s2: number;
+    j1s3: number;
+    j2s3: number;
+    j1s4: number;
+    j2s4: number;
+    j1s5: number;
+    j2s5: number;
 }
