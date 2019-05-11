0bb47b06b008d13d4444d7cfbb52444e8c29dd8b
Esteban Cruz <shirou.k7@gmail.com>
2019-05-11 00:33:08 -0500

    Drag completo

--- src/app/admin/enfrentamiento/enfrentamiento.component.ts
+++ src/app/admin/enfrentamiento/enfrentamiento.component.ts
@@ -42,4 +42,26 @@ export class EnfrentamientoComponent implements OnInit {
         this.listaCompetidores = inscritos.filter(inscrito => this.competencia.id == inscrito.idCompetencia))
     }
 
+      // Drag and Drop
+
+      dragStart(ev) {
+        ev.dataTransfer.setData("Text", ev.target.id);
+        document.getElementById("demo").innerHTML = "Started to drag the p element";
+      }
+      
+       dragEnd(ev) {
+        document.getElementById("demo").innerHTML = "Finished dragging the p element.";
+      }
+      
+       allowDrop(ev) {
+        ev.preventDefault();
+      }
+      
+       drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("Text");
+        ev.target.appendChild(document.getElementById(data));
+        document.getElementById("demo").innerHTML = "The p element was dropped";
+        alert('Usted a seleccionado a un jugador. ');
+      }
 }
