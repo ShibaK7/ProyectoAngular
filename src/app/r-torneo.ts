ab1c4129ba9e0d1addca6deda7a4e8fdff5521a7
Rebeca <reberuqu98@gmail.com>
2019-05-13 07:24:33 -0500

    FormulariosTerminados

--- src/app/r-torneo.ts
+++ src/app/r-torneo.ts
@@ -5,7 +5,7 @@ export class RTorneo {
         public sede: string,
         public cancha: string,
         public descripcion: string,
-        public imagen: string,
+        public financiamiento: string,
         public inscripcionInicio: string,
         public inscripcionFinal: string,
         public torneoInicio: string,
--- src/app/torneos/Torneos-Record.ts
+++ src/app/torneos/Torneos-Record.ts
@@ -15,6 +15,21 @@ export class Torneo {
     imagen2: string;
     imagen3: string;
     imagen4: string;
-  }
+    inscripcionInicio: string;
+    inscripcionFinal: string;
+
+    constructor(nombreTorneo: string, nacionalidad: string, tipoCancha: string, descripcion: string, financiamiento: string, fechaInicio: string, fechaFinal: string, inscripcionInicio: string, inscripcionFinal: string)
+    {
+        this.nombreTorneo = nombreTorneo;
+        this.nacionalidad = nacionalidad;
+        this.tipoCancha = tipoCancha;
+        this.descripcion = descripcion;
+        this.financiamientoTotal = financiamiento;
+        this.fechaInicio = fechaInicio;
+        this.fechaFinal = fechaFinal;
+        this.inscripcionInicio = inscripcionInicio;
+        this.inscripcionFinal = inscripcionFinal;
+    }
+}
 
 
