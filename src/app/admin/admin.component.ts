a909559003f9e2493ffc84cf762af05cc292fbb6
Rebeca <reberuqu98@gmail.com>
2019-05-13 10:44:05 -0500

    Arreglando

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -8,8 +8,6 @@ import { TorneosRecordService } from '../torneos/torneos-record.service';
 
 import { FechaCompetenciaService } from '../fecha-competencia.service';
 
-import { CompetenciaService } from '../competencia.service';
-import { Competencia } from '../competencia';
 
 
 
@@ -21,7 +19,7 @@ import { Competencia } from '../competencia';
 })
 export class AdminComponent implements OnInit {
 
-  private nombres: string;
+  private nombre: string;
   private nacionalidadSeleccionada: string = null;
   private registrosPorPagina: number = 4;
   private numeroPagina: number = 1;
@@ -36,54 +34,19 @@ export class AdminComponent implements OnInit {
 
 
   torneos: Torneo[];
-  competencias: Competencia[];
 
   token;
   usuario;
-  constructor(private competenciaService : CompetenciaService,private torneoService: TorneosRecordService, private route: ActivatedRoute, private location: Location) {
+  constructor(private torneoService: TorneosRecordService, private route: ActivatedRoute, private location: Location) {
     this.token = localStorage.getItem('token');
     this.usuario = localStorage.getItem('correo');
   }
 
   ngOnInit() {
-    /*
-    this.getCompetencias();
-    this.getPaginas();
     this.getTorneos();
     this.getPaginasTorneo();
     this.nombre = this.route.snapshot.url.toString();
-    */
-   this.getCompetencias();
-   this.getPaginas();
-   this.nombre = this.route.snapshot.url.toString();
-  }
-/*
-  getCompetencias(): void {
-    if(!this.filtradoTorneo)
-    {
-      let indice = (this.numeroPagina - 1)*this.registrosPorPagina;
-      this.competenciaService.getCompetencias()
-      .subscribe(competencias =>
-        this.competencias = competencias.filter(competencia => this.torneoSeleccionado == null || this.torneoSeleccionado == competencia.torneo).slice(indice, indice+this.registrosPorPagina))
-    }
-    else
-    {
-      let indice = (this.numeroPaginaFiltrado - 1)*this.registrosPorPagina;
-      this.competenciaService.getCompetencias()
-      .subscribe(competencias =>
-        this.competencias = competencias.filter(competencia => this.torneoSeleccionado == null || this.torneoSeleccionado == competencia.torneo).slice(indice, indice+this.registrosPorPagina))
-    }
-  }
 
-  getPaginas(): void {
-    this.competenciaService.getCompetencias().subscribe( competencias =>
-        {
-          if(((competencias.length/this.registrosPorPagina)-(Math.trunc(competencias.length / this.registrosPorPagina)))==0)
-            this.totalPaginas = Math.trunc(competencias.length / this.registrosPorPagina)
-          else
-            this.totalPaginas = Math.trunc(competencias.length / this.registrosPorPagina)+1
-        }
-      )
   }
 
   getTorneos(): void {
@@ -218,126 +181,4 @@ export class AdminComponent implements OnInit {
         this.getTorneos();
       }
   }
-*/
-getCompetencias(): void {
-  if(!this.filtradoTorneo)
-  {
-    let indice = (this.numeroPagina - 1)*this.registrosPorPagina;
-    this.competenciaService.getCompetencias()
-    .subscribe(competencias =>
-      this.competencias = competencias.filter(competencia => this.torneoSeleccionado == null || this.torneoSeleccionado == competencia.torneo).slice(indice, indice+this.registrosPorPagina))
-  }
-  else
-  {
-    let indice = (this.numeroPaginaFiltrado - 1)*this.registrosPorPagina;
-    this.competenciaService.getCompetencias()
-    .subscribe(competencias =>
-      this.competencias = competencias.filter(competencia => this.torneoSeleccionado == null || this.torneoSeleccionado == competencia.torneo).slice(indice, indice+this.registrosPorPagina))
-  }
-}
-
-getPaginas(): void {
-  this.competenciaService.getCompetencias().subscribe( competencias =>
-      {
-        if(((competencias.length/this.registrosPorPagina)-(Math.trunc(competencias.length / this.registrosPorPagina)))==0)
-          this.totalPaginas = Math.trunc(competencias.length / this.registrosPorPagina)
-        else
-          this.totalPaginas = Math.trunc(competencias.length / this.registrosPorPagina)+1
-      }
-    )
-}
-
-getPaginasFiltradas(): void {
-  this.competenciaService.getCompetencias().subscribe( competencias =>
-    {
-      if(((competencias.filter(competencia => this.torneoSeleccionado == null || this.torneoSeleccionado == competencia.torneo).length/this.registrosPorPagina)-(Math.trunc(competencias.filter(competencia => this.torneoSeleccionado == null || this.torneoSeleccionado == competencia.torneo).length / this.registrosPorPagina)))==0)
-        this.totalPaginasFiltrado = Math.trunc(competencias.filter(competencia => this.torneoSeleccionado == null || this.torneoSeleccionado == competencia.torneo).length / this.registrosPorPagina)
-      else
-        this.totalPaginasFiltrado = Math.trunc(competencias.filter(competencia => this.torneoSeleccionado == null || this.torneoSeleccionado == competencia.torneo).length / this.registrosPorPagina)+1
-    }
-  )
-}
-
-get torneosCompetencias(): string[] {
-  return this.competenciaService.getTorneosCompetencias();
-}
-
-cambiarTorneoSeleccionado(newTorneoSeleccionado?: string) {
-  if(newTorneoSeleccionado=="")
-  {
-    this.torneoSeleccionado = null;
-    this.filtradoTorneo = false;
-    this.numeroPaginaFiltrado = 1;
-    this.getCompetencias();
-  }
-  else
-  {
-    this.torneoSeleccionado = newTorneoSeleccionado;
-    this.filtradoTorneo = true;
-    this.numeroPagina = 1;
-    this.getPaginasFiltradas();
-    this.getCompetencias();
-  }
-}
-
-avanzarPagina() {
-  if(!this.filtradoTorneo)
-  {
-    if(this.numeroPagina < this.totalPaginas)
-      this.numeroPagina++;
-      this.getCompetencias();
-  }
-  else
-  {
-    if(this.numeroPaginaFiltrado < this.totalPaginasFiltrado)
-      this.numeroPaginaFiltrado++;
-      this.getCompetencias();
-  }
-}
-
-regresarPagina() {
-  if(!this.filtradoTorneo)
-  {
-    if(this.numeroPagina > 1)
-    this.numeroPagina--;
-    this.getCompetencias();
-  }
-  else
-  {
-    if(this.numeroPaginaFiltrado > 1)
-    this.numeroPaginaFiltrado--;
-    this.getCompetencias();
-  }
-}
-
-avanzarFinal() {
-  if(!this.filtradoTorneo)
-    {
-      this.numeroPagina = this.totalPaginas;
-      this.getCompetencias();
-    }
-    else
-    {
-      this.numeroPaginaFiltrado = this.totalPaginasFiltrado;
-      this.getCompetencias();
-    }
-}
-
-regresarPrincipio(genero: number) {
-  if(!this.filtradoTorneo)
-    {
-      this.numeroPagina = 1;
-      this.getCompetencias();
-    }
-    else
-    {
-      this.numeroPaginaFiltrado = 1;
-      this.getCompetencias();
-    }
-}
-
-
-
-
-
 }
