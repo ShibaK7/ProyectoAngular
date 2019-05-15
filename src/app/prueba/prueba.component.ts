e87b08d5befa99d91bdc58717f24800fdb0fd325
Esteban Cruz <shirou.k7@gmail.com>
2019-05-15 15:03:18 -0500

    drag and drop actualizado

--- src/app/prueba/prueba.component.ts
+++ src/app/prueba/prueba.component.ts
@@ -28,6 +28,29 @@ export class PruebaComponent implements OnInit {
     consultar(): void{
       window.print();
     }
+
+      // Drag and Drop
+
+      dragStart(ev) {
+        ev.dataTransfer.setData("Text", ev.target.id);
+        document.getElementById("demo").innerHTML = "Started to drag the p element";
+      }
+      
+       dragEnd(ev) {
+        document.getElementById("demo").innerHTML = "Finished dragging the p element.";
+      }
+      
+       allowDrop(ev) {
+        ev.preventDefault();
+      }
+      
+       drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("Text");
+        ev.target.appendChild(document.getElementById(data));
+        document.getElementById("demo").innerHTML = "The p element was dropped";
+        //alert('Usted a seleccionado a un jugador. ');
+      }
   }
 
 
