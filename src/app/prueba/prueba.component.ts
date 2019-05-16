4fc0b87c095f26397754b693c728e3c315a7e09d
Esteban Cruz <shirou.k7@gmail.com>
2019-05-15 21:03:24 -0500

    llaves chido sin diseño

--- src/app/prueba/prueba.component.ts
+++ src/app/prueba/prueba.component.ts
@@ -9,6 +9,9 @@ import { Versus } from 'src/app/versus';
 export class PruebaComponent implements OnInit {
 
   listaAux: Versus[] = [];
+  jugador1:any;
+  jugador2:any;
+
 
   constructor() { }
 
@@ -29,15 +32,36 @@ export class PruebaComponent implements OnInit {
       window.print();
     }
 
+    imprime(uno:any, dos:any):void{
+      this.jugador1 = document.getElementById(uno).textContent;
+      this.jugador2 = document.getElementById(dos).textContent;
+      console.log(this.jugador1, this.jugador2);
+      localStorage.setItem("jugador1",this.jugador1);
+      localStorage.setItem("jugador2",this.jugador2);
+    }
+
+    imprime2(uno:any, dos:any):void{
+      let aux = document.getElementById(uno).lastElementChild;
+      let aux2 = document.getElementById(dos).lastElementChild;
+
+
+      this.jugador1 = aux.innerHTML;
+      this.jugador2 = aux2.innerHTML;
+
+      console.log(this.jugador1, this.jugador2);
+      localStorage.setItem("jugador1",this.jugador1);
+      localStorage.setItem("jugador2",this.jugador2);
+    }
+
       // Drag and Drop
 
       dragStart(ev) {
         ev.dataTransfer.setData("Text", ev.target.id);
-        document.getElementById("demo").innerHTML = "Started to drag the p element";
+        //document.getElementById("demo").innerHTML = "Started to drag the p element";
       }
       
        dragEnd(ev) {
-        document.getElementById("demo").innerHTML = "Finished dragging the p element.";
+        //document.getElementById("demo").innerHTML = "Finished dragging the p element.";
       }
       
        allowDrop(ev) {
@@ -48,7 +72,7 @@ export class PruebaComponent implements OnInit {
         ev.preventDefault();
         var data = ev.dataTransfer.getData("Text");
         ev.target.appendChild(document.getElementById(data));
-        document.getElementById("demo").innerHTML = "The p element was dropped";
+        //document.getElementById("demo").innerHTML = "The p element was dropped";
         //alert('Usted a seleccionado a un jugador. ');
       }
   }
